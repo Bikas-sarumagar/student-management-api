79e46ec8336f091d0b4271cc4560e5a3d57e3d3c
Bikas <magarbikash1002@gmail.com>
2025-08-20 23:25:30 +0545

    feat(student): support bulk create in StudentViewSet
    - Override create() method to allow posting multiple students at once
    - Handles both single object and list of objects
    - Fixed typo in 'ordering_fields'

--- students/views.py
+++ students/views.py
@@ -1,7 +1,7 @@
-from rest_framework import viewsets
+from rest_framework import viewsets, status
 from rest_framework.filters import SearchFilter, OrderingFilter
 from django_filters.rest_framework import DjangoFilterBackend
-
+from rest_framework.response import Response
 from .models import Student
 from .serializers import StudentSerializer
 from .permissions import IsStaffOrReadOnly
@@ -15,3 +15,13 @@ class StudentViewSet(viewsets.ModelViewSet):
     Ordering_fields = ['roll', 'name', 'student_class', "dob"]
     ordering = ['roll']
     permission_classes = [IsStaffOrReadOnly]
+    
+    def create(self, request, *args, **kwargs):
+        # Handle both single object & list of objects
+        is_many = isinstance(request.data, list)
+
+        serializer = self.get_serializer(data=request.data, many=is_many)
+        serializer.is_valid(raise_exception=True)
+        self.perform_create(serializer)
+
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
